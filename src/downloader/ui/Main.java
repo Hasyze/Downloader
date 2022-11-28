a0bdba7dd53af0411b2c97802d876923bb390481
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-11-28 15:03:07 +0100

    Ajout Frame au main

--- src/downloader/ui/Main.java
+++ src/downloader/ui/Main.java
@@ -1,13 +1,26 @@
 package downloader.ui;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-	public static void main(String[] args) {
+public class Main extends JFrame {
+
+	Main(String title, String[] args) {
+		super(title);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 		for (String url : args) {
 			DLThread DL = new DLThread(url);
 			DL.start();
-			DL.getDL();
 		}
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Main("Downloader", args).setVisible(true);
+			}
+		});
 	}
 
 }
