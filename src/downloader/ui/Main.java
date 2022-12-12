a3d83f233b9a928bb15b8c91a865d4b08183e30c
Aziz Khelifi <ahmed-aziz.khelifi@etu.univ-grenoble-alpes.fr>
2022-12-12 14:24:55 +0100

    Add

--- src/downloader/ui/Main.java
+++ src/downloader/ui/Main.java
@@ -1,8 +1,16 @@
 package downloader.ui;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import downloader.fc.Downloader;
@@ -13,16 +21,40 @@ public class Main extends JFrame {
 		super(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(420, 500);
-
+		
 		JPanel pan = new JPanel();
-		pan.setLayout(new BoxLayout(pan, BoxLayout.PAGE_AXIS));
 		this.add(pan);
+		pan.setLayout(new BorderLayout());
+		
+		
+		JPanel panDL = new JPanel();
+		panDL.setLayout(new StackLayout());
+		
+		JPanel panAdd = new JPanel(new BorderLayout());
+		JTextField addUrl = new JTextField("");
+		JButton addButton = new JButton("add");
+		addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String newURL = addUrl.getText();
+				Downloader dler = new Downloader(newURL);
+				dler.execute();
+				Download bar = new Download(dler);
+				panDL.add(bar);
+			}
+		});
+		
+		panAdd.add(BorderLayout.CENTER, addUrl);
+		panAdd.add(BorderLayout.EAST, addButton);
+		
+		pan.add(BorderLayout.CENTER, panDL);
+		pan.add(BorderLayout.SOUTH, panAdd);
 
 		for (String url : args) {
 			Downloader dler = new Downloader(url);
 			dler.execute();
 			Download bar = new Download(dler);
-			pan.add(bar);
+			panDL.add(bar);
 		}
 	}
 
